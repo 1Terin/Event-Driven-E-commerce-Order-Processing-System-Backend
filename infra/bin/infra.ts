9a8a06f33dcb53a721229368bbe24524b5279cf6
Terin <terinchris2005@gmail.com>
2026-01-01 16:30:50 +0530

    updates

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
+import 'dotenv/config';
 
 const app = new cdk.App();
 
 new InfraStack(app, 'OrderProcessingStack', {
-  
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
